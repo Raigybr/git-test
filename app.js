174705089ab584a8ccb3a2b35f42a11d0fcb2955
Kelvin Souza <kelvinsouza369@outlook.com>
2021-07-13 19:33:25 -0300

    primeira pull request

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log('O corno leu isso e o gay tmb')
+
+/* Pra fazer pull request tem que dar fork no projeto
+ai tem permissao pra dar commit etc 
+dps de terminar vai no no seu repo forkado -> contribute -> open pull request*/
